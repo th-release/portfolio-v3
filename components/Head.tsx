c7655f1e03156a13d237dce2527a012912aeb6be
th-release <th.release.cth@gmail.com>
2022-09-25 05:50:08 +0900

    portfolio

---
+++ components/Head.tsx
@@ -0,0 +1,11 @@
+import NextHead from 'next/head'
+
+const Head = () => {
+  return (
+    <NextHead>
+      <title>th-release.cth's portfolio-v3</title>
+      <meta name="description" content="최태혁의 우주짱짱 포트폴리오 v3" />
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  )
+}
